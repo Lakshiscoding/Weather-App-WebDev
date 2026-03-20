25e674268a25eef08116622e9aca595cfe64bfaf
Laksh Vashist <anuraaz2007@gmail.com>
2026-03-20 23:55:44 +0530

    separated files into html, css, and js

---
+++ script.js
@@ -0,0 +1,76 @@
+var currentTheme = localStorage.getItem("themeColor");
+
+if (currentTheme) {
+    document.getElementById("pageBody").className=currentTheme;
+}
+
+function changeTheme(color) {
+    document.getElementById("pageBody").className=color;
+    localStorage.setItem("themeColor",color);
+}
+
+function getWeather() {
+    var city=document.getElementById("cityInput").value;
+    
+    if (city=="") {
+        alert("Please type a city!");
+        return;
+    }
+
+    saveToHistory(city);
+
+    var apiKey="3ebac56ed327bcabde7e56f98ed62493"; 
+    var url="https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apiKey+"&units=metric";
+    
+    var box=document.getElementById("result-box");
+    box.innerHTML="Loading...";
+
+    fetch(url)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            if (data.name) {
+                box.innerHTML="City: "+data.name+"<br> Temperature: "+data.main.temp+" °C";
+            } else {
+                box.innerHTML="City not found!";
+            }
+        })
+        .catch(function(error) {
+            box.innerHTML="Error connecting to the internet!";
+        });
+}
+
+function saveToHistory(city) {
+    var oldHistory=localStorage.getItem("myHistory");
+    
+    if (oldHistory==null) {
+        localStorage.setItem("myHistory",city);
+    } else {
+        localStorage.setItem("myHistory",oldHistory+","+city);
+    }
+    
+    showHistory();
+}
+
+function showHistory() {
+    var list=document.getElementById("historyList");
+    var data=localStorage.getItem("myHistory");
+    
+    list.innerHTML=""; 
+    
+    if (data != null) {
+        var cities=data.split(",");
+        
+        for (var i = 0; i < cities.length; i++) {
+            list.innerHTML+="<li>"+cities[i]+"</li>";
+        }
+    }
+}
+
+function clearHistory() {
+    localStorage.removeItem("myHistory");
+    showHistory();
+}
+
+showHistory();
