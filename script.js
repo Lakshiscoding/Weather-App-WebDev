c54cc1a253501a84b4050b602ee360146290e7a7
Laksh Vashist <anuraaz2007@gmail.com>
2026-03-24 12:13:23 +0530

    separated files and enhanced UI with humidity

--- script.js
+++ script.js
@@ -1,69 +1,64 @@
 var currentTheme = localStorage.getItem("themeColor");
-
 if (currentTheme) {
-    document.getElementById("pageBody").className=currentTheme;
+    document.getElementById("pageBody").className = currentTheme;
 }
 
 function changeTheme(color) {
-    document.getElementById("pageBody").className=color;
-    localStorage.setItem("themeColor",color);
+    document.getElementById("pageBody").className = color;
+    localStorage.setItem("themeColor", color);
 }
 
 function getWeather() {
-    var city=document.getElementById("cityInput").value;
-    
-    if (city=="") {
+    var city = document.getElementById("cityInput").value;
+    if (city == "") {
         alert("Please type a city!");
         return;
     }
 
     saveToHistory(city);
 
-    var apiKey="3ebac56ed327bcabde7e56f98ed62493"; 
-    var url="https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apiKey+"&units=metric";
+    var apiKey = "YOUR_API_KEY_HERE"; 
+    var url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=metric";
     
-    var box=document.getElementById("result-box");
-    box.innerHTML="Loading...";
+    var box = document.getElementById("result-box");
+    box.innerHTML = "Fetching data...";
 
     fetch(url)
-        .then(function(response) {
-            return response.json();
-        })
+        .then(function(response) { return response.json(); })
         .then(function(data) {
             if (data.name) {
-                box.innerHTML="City: "+data.name+"<br> Temperature: "+data.main.temp+" °C";
+                box.innerHTML = "<strong>" + data.name + "</strong><br>" +
+                                "🌡️ Temp: " + data.main.temp + "°C<br>" +
+                                "💧 Humidity: " + data.main.humidity + "%<br>" + 
+                                "☁️ Status: " + data.weather[0].description;
             } else {
-                box.innerHTML="City not found!";
+                box.innerHTML = "City not found!";
             }
         })
         .catch(function(error) {
-            box.innerHTML="Error connecting to the internet!";
+            box.innerHTML = "Connection Error!";
         });
 }
 
 function saveToHistory(city) {
-    var oldHistory=localStorage.getItem("myHistory");
-    
-    if (oldHistory==null) {
-        localStorage.setItem("myHistory",city);
+    var oldHistory = localStorage.getItem("myHistory");
+    if (oldHistory == null) {
+        localStorage.setItem("myHistory", city);
     } else {
-        localStorage.setItem("myHistory",oldHistory+","+city);
+        localStorage.setItem("myHistory", oldHistory + "," + city);
     }
-    
     showHistory();
 }
 
 function showHistory() {
-    var list=document.getElementById("historyList");
-    var data=localStorage.getItem("myHistory");
-    
-    list.innerHTML=""; 
-    
+    var list = document.getElementById("historyList");
+    var data = localStorage.getItem("myHistory");
+    list.innerHTML = ""; 
     if (data != null) {
-        var cities=data.split(",");
-        
-        for (var i = 0; i < cities.length; i++) {
-            list.innerHTML+="<li>"+cities[i]+"</li>";
+        var cities = data.split(",");
+        var lastFive = cities.slice(-5).reverse(); 
+        for (var i = 0; i < lastFive.length; i++) {
+            list.innerHTML += "<li>" + lastFive[i] + "</li>";
         }
     }
 }
